88de30baca273e0a06cd3a99e4dafbf8943ea9c9
Lucas Weber <lucas.weber@cwi.com.br>
2022-11-02 22:49:40 -0300

    Implementado disparo da arma
    Implementado disparo da arma

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [SerializeField] private Spawner bulletSpawner;
+    [SerializeField] private Spawner emptyShellSpawner;
+    [SerializeField] private GameObject muzzleFlashPrefab;
+
+    [SerializeField] float firePower = 1000f;
+
+    private const float EMPTY_SHELL_FORCE = 10f;
+
+    private AudioSource audioSource;
+
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Fire();
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject bullet = bulletSpawner.Spawn();
+        bullet.GetComponent<Rigidbody>().AddForce(transform.forward * firePower);
+
+        GameObject emptyShell = emptyShellSpawner.Spawn();
+        emptyShell.GetComponent<Rigidbody>().AddForce(transform.right * EMPTY_SHELL_FORCE);
+
+        GameObject muzzleFlash = Instantiate(muzzleFlashPrefab, bulletSpawner.transform.position, bulletSpawner.transform.rotation);
+        muzzleFlash.transform.Rotate(new Vector3(0, 180, 0));
+
+        audioSource.Play();
     }
 }
