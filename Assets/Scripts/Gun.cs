e46d672c7618ab6042622b7d594e94de8cafc945
Kaolinhub <kaolinhub@gmail.com>
2022-11-24 20:43:01 -0300

    Rotação da arma

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
+    private Vector2 turn;
+    public float speed = 1f;
+    public int rotation = 5;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,29 @@ public class Gun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            turn.y = turn.y >= 90 ? 90 : turn.y += rotation;
+            transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Debug.Log(turn.y);
+            turn.y = turn.y <= -90 ? -90 : turn.y -= rotation;
+            transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            turn.x = turn.x == 360 ? 0 : turn.x += rotation;
+            transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            turn.x = turn.x == 360 ? 0 : turn.x -= rotation;
+            transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0);
+        }
     }
 }
