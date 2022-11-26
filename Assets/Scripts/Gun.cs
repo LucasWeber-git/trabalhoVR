b521787b8836d335f71173175cca89975c534716
Kaolinhub <kaolinhub@gmail.com>
2022-11-26 14:15:46 -0300

    Rotação da arma

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
-<<<<<<< HEAD
     private Vector2 turn;
     public float speed = 1f;
     public int rotation = 5;
 
     // Start is called before the first frame update
-=======
 
     [SerializeField] private Spawner bulletSpawner;
     [SerializeField] private Spawner emptyShellSpawner;
@@ -22,7 +20,6 @@ public class Gun : MonoBehaviour
 
     private AudioSource audioSource;
 
->>>>>>> 5ef3d6be4d9d6b2f327698cc1f2cea37e2f11743
     void Start()
     {
         audioSource = GetComponent<AudioSource>();        
@@ -30,7 +27,6 @@ public class Gun : MonoBehaviour
 
     void Update()
     {
-<<<<<<< HEAD
         if (Input.GetKey(KeyCode.UpArrow))
         {
             turn.y = turn.y >= 90 ? 90 : turn.y += rotation;
@@ -55,7 +51,7 @@ public class Gun : MonoBehaviour
             turn.x = turn.x == 360 ? 0 : turn.x -= rotation;
             transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0);
         }
-=======
+
         if (Input.GetButtonDown("Fire1"))
         {
             Fire();
@@ -74,6 +70,5 @@ public class Gun : MonoBehaviour
         muzzleFlash.transform.Rotate(new Vector3(0, 180, 0));
 
         audioSource.Play();
->>>>>>> 5ef3d6be4d9d6b2f327698cc1f2cea37e2f11743
     }
 }
